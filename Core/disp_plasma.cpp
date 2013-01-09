fc95230d5bca5ada33fbd371f25c89848a26ac29
Harry Denholm <harry.denholm@gmail.com>
2013-01-09 22:28:48 +0000

    added plasma effect back in

---
+++ Core/disp_plasma.cpp
@@ -0,0 +1,88 @@
+/*
+                 __    ___ __       
+      ____  ____/ /___/ (_) /___  __
+     / __ \/ __  / __  / / __/ / / /    [ LED matrix VFX core ] 
+    / /_/ / /_/ / /_/ / / /_/ /_/ / 
+    \____/\__,_/\__,_/_/\__/\__, /     [ john & harry ] [ 2012 ]
+                           /____/   
+*/
+
+#include "oddity.h"
+#include "vfx.h"
+
+struct EffectData
+{
+  Fix16 step;
+  Fix16 attractorScales[4];
+
+  bool mutations[16];
+};
+
+#ifdef _MSC_VER
+static_assert(sizeof(EffectData) < Constants::MemoryPool, "EffectData too big for global memory pool");
+#endif // _MSC_VER
+
+// ---------------------------------------------------------------------------------------------------------------------
+void plasma_init(FXState& state)
+{
+  EffectData* data = (EffectData*)state.store;
+
+  data->step = (fix16_t)state.rng.genUInt32(0, 0xffffff);
+
+  for (int i=0; i<16; i++)
+    data->mutations[i] = state.rng.genBool();
+
+  for (int i=0; i<4; i++)
+    data->attractorScales[i] = (fix16_t)state.rng.genUInt32(0, 0xfffff);
+}
+
+
+// ---------------------------------------------------------------------------------------------------------------------
+bool plasma_tick(FrameOutput& output, FXState& state)
+{
+  EffectData* data = (EffectData*)state.store;
+
+
+  byte r, g;
+  Fix16 mid(8.0f);
+
+  Fix16 tx1, ty1, tx2, ty2;
+
+  Fix16 XM1 = data->step.sinMV(0.3f, 3.0f) * data->attractorScales[0];
+  Fix16 YM1 = data->step.sinMV(0.1f, 2.0f) * data->attractorScales[1];
+  Fix16 XM2 = data->step.cosMV(0.2f, -3.0f) * data->attractorScales[2];
+  Fix16 YM2 = data->step.cosMV(0.05f, -5.0f) * data->attractorScales[3];
+
+  tx1 = mid + (data->step.sinV(-0.25f) * XM1);
+  ty1 = mid + (data->step.cosV(0.1f) * YM1);
+  tx1 = mid + (data->step.sinMV(-0.5f, 4.0f) * XM2);
+  ty1 = mid + (data->step.cosV(0.1f) * YM2);
+
+  for (int y=0; y<Constants::FrameHeight; y++)
+  {
+    for (int x=0; x<Constants::FrameWidth; x++)
+    {
+      Fix16 fx((float)x), fy((float)y);
+
+      fx += fx.sinMV(0.3f, YM1);
+      fy += fy.cosMV(0.3f, XM2);
+
+      Fix16 dist1 = DistanceBetween(fx, fy, tx1, ty1);
+      Fix16 dist2 = DistanceBetween(fx, fy, tx2, ty2);
+
+      Fix16 tn = Fix16::atan2(dist1, dist2) * ((data->step.cosM(0.25f) + 2.0f) * 0.8f);
+
+      tn += data->step * 0.4f;
+
+      tn += fx.cosMV(0.1f, YM2) * (data->step.sinMV(0.1f, 25.0f) * 0.3f);
+      tn -= fy.sinMV(0.1f, XM1) * (data->step.cosMV(0.1f, 15.0f) * 0.3f);
+
+      ColourGradient(tn, data->mutations[0], data->mutations[1], r, g);
+      setLED(output.frame, x, y, r, g);
+    }
+  }
+
+  data->step += 0.1f;
+
+  return true;
+}
--- Core/oddity.cpp
+++ Core/oddity.cpp
@@ -14,7 +14,8 @@
 // ---------------------------------------------------------------------------------------------------------------------
 // list of all known display modes (fx modules)
 #define DISP(_act) \
-  _act(ripple)
+  _act(ripple) \
+  _act(plasma)
 
 
 // ---------------------------------------------------------------------------------------------------------------------
@@ -27,9 +28,9 @@
   _disp,
 
 #define DOINIT(_disp) \
-  case _disp: _disp##_init(state);
+  case _disp: _disp##_init(state); break;
 #define DOTICK(_disp) \
-  case _disp: _disp##_tick(output, state);
+  case _disp: _disp##_tick(output, state); break;
 
 DISP(PREDEC)
 struct DisplayMode
@@ -44,14 +45,18 @@ struct DisplayMode
   {
     switch (e)
     {
-      DISP(DOINIT)      
+      DISP(DOINIT)
+      default:
+        break;
     }
   }
   static void doTickFor(Enum e, FrameOutput& output, FXState& state)
   {
     switch (e)
     {
-      DISP(DOTICK)      
+      DISP(DOTICK)
+      default:
+        break;
     }
   }
 };
@@ -77,7 +82,37 @@ void init(FXState& state)
 // ---------------------------------------------------------------------------------------------------------------------
 bool tick(const FrameInput& input, FXState& state, FrameOutput& output)
 {
-  DisplayMode::doTickFor(gCurrentDisplayMode, output, state);
+  //DisplayMode::doTickFor(gCurrentDisplayMode, output, state);
+
+
+  Fix16 pT = fix16_mul(fix16_from_int(state.counter), fix16_from_float(0.35f));
+  Fix16 pY = fix16_mul(fix16_from_int(state.counter), fix16_from_float(0.1f));
+
+  for (uint32_t x=0; x<Constants::FrameWidth; x++)
+  {
+    Fix16 pX = fix16_mul(fix16_from_int(x), fix16_from_float(0.3f));
+    pX += pT;
+
+    Fix16 row = Perlin2(pX, pY);
+    row *= 17.0f;
+
+    byte A[] = { 1, 2, 3, 3, 3, 3, 2, 1, 0 };
+    byte B[] = { 0, 0, 0, 1, 2, 3, 3, 3, 3 };
+
+    setLED(output.frame, x, 8, 1, 0);
+
+    int rowInt = row.asInt();
+    if (rowInt < 0)
+      rowInt = -rowInt;
+    if (rowInt > 8)
+      rowInt = 8;
+
+    for (uint32_t y=0; y<rowInt; y++)
+    {
+      setLED(output.frame, x, 8 - y, A[y], B[y]);
+      setLED(output.frame, x, 8 + y, A[y], B[y]);
+    }
+  }
 
   return true;
 }
